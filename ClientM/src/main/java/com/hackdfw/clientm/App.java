1e85a66e15423aca67c9061b10be8b014ecce137
AlexTheCoder <alexjmeech@gmail.com>
2019-02-16 11:53:50 -0600

    Initial push of client

---
+++ ClientM/src/main/java/com/hackdfw/clientm/App.java
@@ -0,0 +1,13 @@
+package com.hackdfw.clientm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
